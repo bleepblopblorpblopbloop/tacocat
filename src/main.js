cc84e5cdeae00d2c3a63e615ffcc1706908fce36
Kyle Chorley <kylechorley@Kyles-MacBook-Pro.local>
2019-10-28 10:12:39 +0100

    catLady png filename change

--- src/main.js
+++ src/main.js
@@ -3,63 +3,54 @@ let beer;
 let ryeAndCoke;
 let martini;
 let pizza;
-// let owner;
-
+// let catLady;
 
 function preload() {
-    player.preload()
-    glasses.preload()
-    catLady.preload()
+  player.preload();
+  glasses.preload();
+  catLady.preload();
 }
 
 function keyPressed() {
-    player.keyPressed()
+  player.keyPressed();
 }
 const game = new Game();
 const glasses = new Glasses();
 const catLady = new CatLady();
 
 function setup() {
-    let canvas = createCanvas(WIDTH, WIDTH);
-    canvas.parent("canvas");
+  let canvas = createCanvas(WIDTH, WIDTH);
+  canvas.parent("canvas");
 }
 
-
-glasses.setRandomPosition()
-catLady.setRandomPosition()
+glasses.setRandomPosition();
+catLady.setRandomPosition();
 
 // ***this appears to do nothing at all***
 // if (glasses.col === player.col) {
 //     glasses.setRandomPosition()
 // }
 
-
-
-
 function draw() {
+  game.drawGrid();
 
-    game.drawGrid();
+  player.draw();
 
-    player.draw();
+  if (glasses.col === player.col && glasses.row === player.row) {
+    glasses.setRandomPosition();
+  }
 
+  glasses.drawGlasses();
 
-    if (glasses.col === player.col && glasses.row === player.row) {
-        glasses.setRandomPosition();
-    }
+  catLady.drawCatLady();
 
-    glasses.drawGlasses();
+  // this ensures that the drink and the catLady do not end up in the same position
+  if (catLady.col === glasses.col && catLady.row === glasses.row) {
+    catLady.setRandomPosition();
+  }
 
-    catLady.drawCatLady();
-
-    // this ensures that the drink and the catLady do not end up in the same position
-    if (catLady.col === glasses.col && catLady.row === glasses.row) {
-        catLady.setRandomPosition();
-    }
-
-    // this ensures that the player and catLady  can never occupy the same position
-    if (catLady.col === player.col && catLady.row === player.row) {
-        catLady.setRandomPosition();
-    }
-
-
+  // this ensures that the player and catLady  can never occupy the same position
+  if (catLady.col === player.col && catLady.row === player.row) {
+    catLady.setRandomPosition();
+  }
 }
