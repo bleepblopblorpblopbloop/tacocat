3cb055c63ce1bd92a97047c34d4c0e0bb4105b00
Kyle Chorley <kylechorley@Kyles-MacBook-Pro.local>
2019-10-28 12:11:37 +0100

    added second square to borad for table

--- src/game.js
+++ src/game.js
@@ -2,6 +2,7 @@ class Game {
     drawGrid() {
         strokeWeight(2)
         rect(0, 0, WIDTH, HEIGHT)
+        rect((0 + (WIDTH * 0.1)), (0 + (HEIGHT * 0.1)), WIDTH * 0.8, HEIGHT * 0.8)
     }
 
 }
