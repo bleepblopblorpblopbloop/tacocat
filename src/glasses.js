770e99ccfddecde415e5dc71654c883f29e4688f
Kyle Chorley <kylechorley@Kyles-MacBook-Pro.local>
2019-10-29 16:18:03 +0100

    multiple glasses - cant be in same space - points

--- src/glasses.js
+++ src/glasses.js
@@ -1,25 +1,39 @@
 class Glasses {
-    constructor() {
+    // constructor(type) {
+    constructor(drink) {
         // this sets a random position within a pre-defined range - allows us to have the glasses appear inside the edge of the table
-        this.setRandomPosition(1, 8)
+
+        this.drinks = drink;
+        // this.positions = [];
+        // this.img = []
+
+        this.setRandomPosition(1, 8);
     }
 
     // this seets the intial position of the beer to a random position on the board
     setRandomPosition(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        this.col = Math.floor((Math.random() * (max - min + 1)) + min) * (SQUARE_SIDE);
-        this.row = Math.floor((Math.random() * (max - min + 1)) + min) * (SQUARE_SIDE);
+
+
+        this.col = Math.floor((Math.random() * (max - min + 1)) + min) * (SQUARE_SIDE)
+        this.row = Math.floor((Math.random() * (max - min + 1)) + min) * (SQUARE_SIDE)
+
+
     }
 
 
     preload() {
-        this.beer = loadImage("../assets/beermug.png")
+        // let randDrink = this.drinks[Math.floor(Math.random() * this.drinks.length)]
+
+        this.img = loadImage(`../assets/${this.drinks}.png`)
+
+        // console.log(randDrink)
+        // console.log()
     }
 
     // positions the glass on the grid
     drawGlasses() {
-        image(this.beer, this.col, this.row, SQUARE_SIDE, SQUARE_SIDE)
+        console.log(this.img);
+        this.img && image(this.img, this.col, this.row, SQUARE_SIDE, SQUARE_SIDE)
     }
 
     // this moves the glass in the direction of the keyPress
--- src/main.js
+++ src/main.js
@@ -3,10 +3,15 @@ let beer;
 let ryeAndCoke;
 let martini;
 let pizza;
+// const beerGlass = new Glasses();
+
 
 function preload() {
     player.preload();
     glasses.preload();
+    glasses2.preload();
+    glasses3.preload();
+
     catLady.preload();
 }
 
@@ -16,7 +21,13 @@ function keyPressed() {
 
 
 const game = new Game();
-const glasses = new Glasses();
+
+
+const glasses = new Glasses('matini');
+const glasses2 = new Glasses('beermug');
+const glasses3 = new Glasses('rye_and_coke');
+
+
 let catLady = new CatLady();
 
 
@@ -35,17 +46,43 @@ function draw() {
 
     player.draw();
 
+    // this allows tacocat to push the drinks
     if (glasses.col === player.col && glasses.row === player.row) {
         glasses.keyPressed();
+    } else if (glasses2.col === player.col && glasses2.row === player.row) {
+        glasses2.keyPressed();
+    } else if (glasses3.col === player.col && glasses3.row === player.row) {
+        glasses3.keyPressed();
     }
 
+    // draws the different glasses and catLady on the board
     glasses.drawGlasses();
+    glasses2.drawGlasses();
+    glasses3.drawGlasses();
+
+    // prevents the different drinks from occupying the same space
+    if (glasses.col === glasses2.col && glasses.row === glasses2.row) {
+        glasses2.setRandomPosition();
+    } else if (glasses.col === glasses3.col && glasses.row === glasses3.row) {
+        glasses3.setRandomPosition();
+    } else if (glasses2.col === glasses3.col && glasses2.row === glasses3.row) {
+        glasses3.setRandomPosition();
+    }
+
+
+
     catLady.drawCatLady();
 
 
     // this ensures that the drink and the catLady do not end up in the same position
     if (catLady.col === glasses.col && catLady.row === glasses.row) {
         glasses.setRandomPosition(1, 8);
+        counter -= 100;
+    } else if (catLady.col === glasses2.col && catLady.row === glasses2.row) {
+        glasses2.setRandomPosition(1, 8);
+        counter -= 100;
+    } else if (catLady.col === glasses3.col && catLady.row === glasses3.row) {
+        glasses3.setRandomPosition(1, 8);
         counter -= 100;
     }
 
@@ -54,16 +91,19 @@ function draw() {
     if (catLady.col === player.col && catLady.row === player.row) {
         // catLady.setRandomPosition(1, 8);
         if (catLady.movementArr[0] === 'right') {
-            catLady.col -= (SQUARE_SIDE * 2);
-        } else if (catLady.movementArr[0] === 'down') {
-            catLady.row -= (SQUARE_SIDE * 2);
-        } else if (catLady.movementArr[0] === 'left') {
-            catLady.col += (SQUARE_SIDE * 2);
-        } else if (catLady.movementArr[0] === 'up') {
             catLady.row += (SQUARE_SIDE * 2);
+            catLady.col += (SQUARE_SIDE);
+        } else if (catLady.movementArr[0] === 'down') {
+            catLady.col += (SQUARE_SIDE * 2);
+            catLady.row += (SQUARE_SIDE);
+        } else if (catLady.movementArr[0] === 'left') {
+            catLady.row -= (SQUARE_SIDE * 2);
+            catLady.col -= (SQUARE_SIDE);
+        } else if (catLady.movementArr[0] === 'up') {
+            catLady.col -= (SQUARE_SIDE * 2);
+            catLady.row -= (SQUARE_SIDE);
         }
-        console.log(this);
-        // catLady.drawCatLady();
+
     }
 
 
@@ -71,9 +111,15 @@ function draw() {
     if (glasses.col <= 79 || glasses.col >= 720 || glasses.row <= 79 || glasses.row >= 720) {
         glasses.setRandomPosition(1, 8);
         counter += 100;
+    } else if (glasses2.col <= 79 || glasses2.col >= 720 || glasses2.row <= 79 || glasses2.row >= 720) {
+        glasses2.setRandomPosition(1, 8);
+        counter += 100;
+    } else if (glasses3.col <= 79 || glasses3.col >= 720 || glasses3.row <= 79 || glasses3.row >= 720) {
+        glasses3.setRandomPosition(1, 8);
+        counter += 100;
     }
 
 
     console.log(counter)
-    console.log(catLady.movementArr)
+    // console.log(catLady.movementArr)
 }
